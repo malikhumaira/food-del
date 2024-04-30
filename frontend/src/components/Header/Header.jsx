9cc08316525cc45bba08e08e2d79275750021d82
Humaira Malik <malik.humaira138@gmail.com>
2024-04-30 15:08:04 +0200

    initial commit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="header">
+        <div className="header-contents">
+            <h2>Order your favorite food here</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem impedit corrupti minima laboriosam possimus aliquam suscipit recusandae earum deleniti, expedita enim repellendus! Distinctio labore harum similique rem veritatis, error adipisci!</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
